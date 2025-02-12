bb48834d64f7d4199149fa182bffff63794a3682
Hari-singh-Joshi <132601298+Hari-singh-Joshi@users.noreply.github.com>
2025-02-12 11:42:11 +0530

    update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from . import views
 urlpatterns = [
-    
-  path()
+    path('create/', views.CreateView, name='create_product'),
+    path('search/<int:product_id>/', views.SearchView, name='search_product'),
+    path('update/<int:product_id>/', views.UpdateView, name='update_product'),
+    path('delete/<int:product_id>/', views.DeleteView, name='delete_product'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
