6329bd561fe488d9642fcebae49d6e7cb76f55bc
Hari-singh-Joshi <132601298+Hari-singh-Joshi@users.noreply.github.com>
2025-02-12 11:39:45 +0530

    views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from models import Product
+
+def CreateView(name, category, sku, description, price, stock_quantity):
+    product = Product(
+        name=name,
+        category=category,
+        sku=sku,
+        description=description,
+        price=price,
+        stock_quantity=stock_quantity
+    )
+    product.save()
+    return product
+
+def SearchView(product_id):
+    return Product.objects.get(id=product_id)
+
+def UpdateView(product_id, **kwargs):
+    Product.objects.filter(id=product_id).update(**kwargs)
+
+def DeleteView(product_id):
+    Product.objects.filter(id=product_id).delete()
